0ee19e6ed789132f1184b9d53bf54ee844859530
Jon-Paul Da Prato <jon-pauldaprato@Jon-Pauls-MacBook-Pro-2.local>
2022-03-22 15:00:07 -0500

    Create multiple package POC

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-import { ExampleComponent } from 'adk-react-library'
-import 'adk-react-library/dist/index.css'
+import { POCPackage1 } from 'adk-react-library/poc-package-1'
+import { POCPackage2 } from 'adk-react-library/poc-package-2'
+import 'adk-react-library/poc-package-1/dist/index.css'
+import 'adk-react-library/poc-package-2/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div>
+      <POCPackage1 text='Create React Library Example #1 😄' />
+      <POCPackage2 text='Create React Library Example #2 😄' />
+    </div>
+  )
 }
 
 export default App
