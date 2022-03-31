295562d3a670011d87f1cd45bb08d21687fc9419
Betty Lam <[replace email]>
2022-03-31 15:45:50 -0700

    Add more functionality/customization to button component, add stories for Button

--- Button/index.js
+++ Button/index.js
@@ -1,6 +1,45 @@
 import React from 'react'
 import Button from '@mui/material/Button'
 
-export const ButtonComponent = ({ label }) => (
-  <Button variant='outlined'>{label}</Button>
-)
+export const buttonVariant = {
+  contained: 'contained',
+  outlined: 'outlined',
+  text: 'text'
+}
+
+export const buttonSize = {
+  small: 'small',
+  medium: 'medium',
+  large: 'large'
+}
+
+export const buttonSx = {
+  blue: {
+    color: 'white',
+    backgroundColor: '#1890ff'
+  },
+  gray: {
+    color: 'white',
+    backgroundColor: 'lightgray',
+    '&:hover': { backgroundColor: 'darkgray' }
+  }
+}
+
+export const defaultProps = {
+  variant: buttonVariant.contained,
+  size: buttonSize.medium
+}
+
+export const ButtonComponent = (props) => {
+  const buttonProps = { ...defaultProps, ...props }
+  delete buttonProps.log
+  const onClickFn = () => {
+    props.log()
+    props.onClick()
+  }
+  return (
+    <Button {...buttonProps} onClick={onClickFn}>
+      {props.children}
+    </Button>
+  )
+}
