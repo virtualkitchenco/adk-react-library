4712f8ce146cdc32e8257470bf13f9f6f5445476
Jon-Paul Da Prato <jon-pauldaprato@Jon-Pauls-MacBook-Pro-2.local>
2022-03-31 09:15:43 -0500

    Implement MUI basic table component example

--- example/src/App.js
+++ example/src/App.js
@@ -3,10 +3,30 @@ import React from 'react'
 import { POCPackage1 } from 'adk-react-library/poc-package-1'
 import { POCPackage2 } from 'adk-react-library/poc-package-2'
 import { ButtonComponent as Button } from 'adk-react-library/Button'
-import { Table } from 'adk-react-library/table'
+import { TableComponent as Table } from 'adk-react-library/table'
 import 'adk-react-library/poc-package-1/dist/index.css'
 import 'adk-react-library/poc-package-2/dist/index.css'
 import 'adk-react-library/Button/dist/index.css'
+
+const createData = (name, calories, fat, carbs, protein) => {
+  return { name, calories, fat, carbs, protein }
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9)
+]
+
+const columns = [
+  'Dessert (100g serving)',
+  'Calories',
+  'Fat (g)',
+  'Carbs (g)',
+  'Protein (g)'
+]
 
 const App = () => {
   return (
@@ -14,7 +34,7 @@ const App = () => {
       <Button label='A Button' />
       <POCPackage1 text='Create React Library Example #1 😄' />
       <POCPackage2 text='Create React Library Example #2 😄' />
-      <Table text='Table Component' />
+      <Table rows={rows} columns={columns} />
     </div>
   )
 }
--- table/index.js
+++ table/index.js
@@ -1,8 +1,42 @@
-import React from 'react'
-import Typography from '@mui/material/Typography'
+import * as React from 'react'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
 
-export const Table = ({ text }) => (
-  <Typography variant='h1' component='div'>
-    {text}
-  </Typography>
-)
+export const TableComponent = ({ columns = [], rows = [] }) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label='adk-table'>
+        <TableHead>
+          <TableRow>
+            {columns.map((column, index) => (
+              <TableCell key={index} align={index === 0 ? 'left' : 'right'}>
+                {column}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component='th' scope='row'>
+                {row.name}
+              </TableCell>
+              <TableCell align='right'>{row.calories}</TableCell>
+              <TableCell align='right'>{row.fat}</TableCell>
+              <TableCell align='right'>{row.carbs}</TableCell>
+              <TableCell align='right'>{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
