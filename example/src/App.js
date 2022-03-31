174d0441543ed6eadcada07e968cd059c2243a8f
Jon-Paul Da Prato <jon-pauldaprato@Jon-Pauls-MacBook-Pro-2.local>
2022-03-31 08:26:48 -0500

    Lay foundation for table component

--- example/src/App.js
+++ example/src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { POCPackage1 } from 'adk-react-library/poc-package-1'
 import { POCPackage2 } from 'adk-react-library/poc-package-2'
 import { ButtonComponent as Button } from 'adk-react-library/Button'
+import { Table } from 'adk-react-library/table'
 import 'adk-react-library/poc-package-1/dist/index.css'
 import 'adk-react-library/poc-package-2/dist/index.css'
 import 'adk-react-library/Button/dist/index.css'
@@ -13,6 +14,7 @@ const App = () => {
       <Button label='A Button' />
       <POCPackage1 text='Create React Library Example #1 😄' />
       <POCPackage2 text='Create React Library Example #2 😄' />
+      <Table text='Table Component' />
     </div>
   )
 }
---
+++ table/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+
+export const Table = ({ text }) => (
+  <Typography variant='h1' component='div'>
+    {text}
+  </Typography>
+)
