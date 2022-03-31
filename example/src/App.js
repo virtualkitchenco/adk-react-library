bec9d959ba381b637dcff1c3063d191368808a62
Jon-Paul Da Prato <jon-pauldaprato@Jon-Pauls-MacBook-Pro-2.local>
2022-03-31 10:19:56 -0500

    Update how rows prop is handled

--- example/src/App.js
+++ example/src/App.js
@@ -3,21 +3,17 @@ import React from 'react'
 import { POCPackage1 } from 'adk-react-library/poc-package-1'
 import { POCPackage2 } from 'adk-react-library/poc-package-2'
 import { ButtonComponent as Button } from 'adk-react-library/Button'
-import { TableComponent as Table } from 'adk-react-library/table'
+import { Table } from 'adk-react-library/table'
 import 'adk-react-library/poc-package-1/dist/index.css'
 import 'adk-react-library/poc-package-2/dist/index.css'
 import 'adk-react-library/Button/dist/index.css'
 
-const createData = (name, calories, fat, carbs, protein) => {
-  return { name, calories, fat, carbs, protein }
-}
-
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9)
+  ['Frozen yoghurt', 159, 6.0, 24, 4.0],
+  ['Ice cream sandwich', 237, 9.0, 37, 4.3],
+  ['Eclair', 262, 16.0, 24, 6.0],
+  ['Cupcake', 305, 3.7, 67, 4.3],
+  ['Gingerbread', 356, 16.0, 49, 3.9]
 ]
 
 const columns = [
--- table/index.js
+++ table/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import Table from '@mui/material/Table'
+import MaterialTable from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
@@ -7,10 +7,10 @@ import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
 
-export const TableComponent = ({ columns = [], rows = [] }) => {
+export const Table = ({ columns = [], rows = [] }) => {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label='adk-table'>
+      <MaterialTable sx={{ minWidth: 650 }} aria-label='adk-table'>
         <TableHead>
           <TableRow>
             {columns.map((column, index) => (
@@ -21,22 +21,25 @@ export const TableComponent = ({ columns = [], rows = [] }) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {rows.map((row, rowIndex) => (
             <TableRow
-              key={row.name}
+              key={rowIndex}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component='th' scope='row'>
-                {row.name}
-              </TableCell>
-              <TableCell align='right'>{row.calories}</TableCell>
-              <TableCell align='right'>{row.fat}</TableCell>
-              <TableCell align='right'>{row.carbs}</TableCell>
-              <TableCell align='right'>{row.protein}</TableCell>
+              {row.map((value, valIndex) => (
+                <TableCell
+                  key={valIndex}
+                  component={valIndex === 0 ? 'th' : ''}
+                  scope={valIndex === 0 ? 'row' : ''}
+                  align={valIndex === 0 ? '' : 'right'}
+                >
+                  {value}
+                </TableCell>
+              ))}
             </TableRow>
           ))}
         </TableBody>
-      </Table>
+      </MaterialTable>
     </TableContainer>
   )
 }
